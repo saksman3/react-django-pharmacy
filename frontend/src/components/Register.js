2aea72e23e7d6aa6b8f01b74ecfc5604893d8983
saksman3 <saksman3@gmail.com>
2022-04-02 00:40:16 +0200

    comment phone css

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 /* import PhoneInput from 'react-phone-input-2' */
-import 'react-phone-input-2/lib/style.css'
+/* import 'react-phone-input-2/lib/style.css' */
 import {
     Button,
     Form,
